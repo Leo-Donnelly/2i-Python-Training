36c443b486230124894da26b963ad8476a0522f6
Leo-Donnelly <leo.donnelly@2itesting.com>
2025-12-09 12:06:27 +0000

    Intermediate Module 1&2 completed

--- Updated Python Course/Intermediate/Module 1/FavouriteThingsPart1.py
+++ Updated Python Course/Intermediate/Module 1/FavouriteThingsPart1.py
@@ -1,6 +1,20 @@
-def favourite_things():
-    def wrapper(**args):
-        if 
+def favourite_things(*args):
+
+    favourite_things = args
+
+    for x, i in enumerate(favourite_things):
+        if i.endswith("s"):
+            verb = "are"
+        else:
+            verb = "is"
+
+        if x == 0:
+            print("One of my favourite things "+verb+" "+i)
+        else:
+            print("Another of my favourite things "+verb+" "+i)
+
+    print("These are " + str(len(favourite_things))+ "of my favourite things")
+        
 
 
 
